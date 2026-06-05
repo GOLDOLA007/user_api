d844637495c9ec7f74ea598dd15b4bf4100bdabb
Kaua <kauaoliveira18062007@gmail.com>
2026-06-05 13:48:00 -0300

    feat: create login request class

---
+++ src/main/java/com/api/user_api/dto/LoginRequest.java
@@ -0,0 +1,12 @@
+package com.api.user_api.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest (
+        @NotBlank (message = "E-mail is required")
+        @Email (message = "Invalid e-mail address")
+        String email,
+        @NotBlank (message = "Password is required")
+        String password
+){}
