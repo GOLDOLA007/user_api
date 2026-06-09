93c3b0c8a0b84b0265f9d2b3bef039c484970d60
Kaua <kauaoliveira18062007@gmail.com>
2026-06-09 13:49:46 -0300

    feat: create TokenService logic

--- src/main/java/com/api/user_api/dto/LoginRequest.java
+++ src/main/java/com/api/user_api/dto/LoginRequest.java
@@ -3,10 +3,10 @@ package com.api.user_api.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record LoginRequest (
-        @NotBlank (message = "E-mail is required")
-        @Email (message = "Invalid e-mail address")
+public record LoginRequest(
+        @NotBlank(message = "E-mail is required")
+        @Email(message = "Invalid e-mail address")
         String email,
-        @NotBlank (message = "Password is required")
+        @NotBlank(message = "Password is required")
         String password
 ){}
