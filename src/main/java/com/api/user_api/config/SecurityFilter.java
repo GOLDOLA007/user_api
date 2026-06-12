700f15a701c27c114ee4d5fe3f945196bb74916c
Kaua <kauaoliveira18062007@gmail.com>
2026-06-12 12:21:51 -0300

    feat: add SecurityFilter

---
+++ src/main/java/com/api/user_api/config/SecurityFilter.java
@@ -0,0 +1,51 @@
+package com.api.user_api.config;
+
+import com.api.user_api.repository.UserRepository;
+import com.api.user_api.service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
+        String token = this.recoverToken(request);
+
+        if(token != null){
+            String email = tokenService.validateToken(token);
+
+            if(!email.isEmpty()){
+                UserDetails user = userRepository.findByEmail(email);
+
+                var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+
+            filterChain.doFilter(request, response);
+        }
+    }
+
+    private String recoverToken(HttpServletRequest request){
+        String authHeader = request.getHeader("Authorization");
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            return null;
+        }
+        return authHeader.replace("Bearer ", "");
+    }
+}
