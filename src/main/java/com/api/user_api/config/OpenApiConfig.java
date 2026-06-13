4e8467aceb4815583da70b715ac9239122c48b97
Kaua <kauaoliveira18062007@gmail.com>
2026-06-13 14:15:19 -0300

    chore: add @EnableAutoConfiguration

--- src/main/java/com/api/user_api/config/OpenApiConfig.java
+++ src/main/java/com/api/user_api/config/OpenApiConfig.java
@@ -2,10 +2,12 @@ package com.api.user_api.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableAutoConfiguration
 public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
