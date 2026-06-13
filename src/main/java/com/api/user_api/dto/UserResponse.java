c5923bf984aaefcc58edf5f6ed10c301c381f341
Kaua <kauaoliveira18062007@gmail.com>
2026-06-13 14:18:07 -0300

    feat: show user's authorities and password in the UserResponse

--- src/main/java/com/api/user_api/dto/UserResponse.java
+++ src/main/java/com/api/user_api/dto/UserResponse.java
@@ -1,9 +1,12 @@
 package com.api.user_api.dto;
 
 import com.api.user_api.model.User;
+import org.springframework.security.core.GrantedAuthority;
 
-public record UserResponse(Long id, String name, String email) {
+import java.util.Collection;
+
+public record UserResponse(Long id, String name, String email, Collection<? extends GrantedAuthority> auth, String password) {
     public UserResponse(User user) {
-        this(user.getId(), user.getName(), user.getEmail());
+        this(user.getId(), user.getName(), user.getEmail(), user.getAuthorities(), user.getPassword());
     }
 }
